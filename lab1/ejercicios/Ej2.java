4ab3f5ddd8c224f3125f3d949a96d3bbe32465f1
Traveller-5040 <gabrielcruztorreblanca@gmail.com>
2026-08-21 17:03:06 -0500

    Create Ej2.java for number input validation
    Implement Ej2.java to collect and validate user input numbers.

---
+++ lab1/ejercicios/Ej2.java
@@ -0,0 +1,43 @@
+package ejercicios;
+// Se importan las librerias necesarias para crear el programa
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        // Se declara el vector
+        Vector<Integer> Numeros = new Vector<>();
+        // Se declaran las variables (enteras) en 0
+        int num = 0;
+        int cont = 0;
+
+        Scanner tec = new Scanner(System.in);
+
+        // Inicia el candado (while)
+        while(cont < 10)
+        {
+            // Se solicita al usuario ingresar un numero
+            System.out.println("Ingrese un numero -> ");
+            num = tec.nextInt();
+
+            // Antes de agregar el numero al vector se verifica que sea mayor al anterior
+            if(cont == 0 || num >= Numeros.get(cont-1)  ) // De ser el primero se obvia la verificacion
+            {
+                Numeros.add(num);
+                cont += 1;
+                continue;
+            } else
+            {
+                // De ser falso se envia el mensaje de error y continua el candado
+                System.out.println("Error, el numero ingresado debe ser mayor al anterior: " + Numeros.get(cont-1));
+                continue;
+            }
+
+        }
+
+        // Se cierra el Scanner y se imprime el Vector
+        tec.close();
+        System.out.print("Numeros ingresados -> " + Numeros);
+    }
+
+}
