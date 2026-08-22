27d756fe9c550d030eb0bc597d46110730e2f3e3
Traveller-5040 <gabrielcruztorreblanca@gmail.com>
2026-08-22 00:00:09 -0500

    Create Act5_a.java

---
+++ lab1/actividades/Act5_a.java
@@ -0,0 +1,11 @@
+package actividades;
+
+public class Act5 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+// Imprimir el arreglo
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
