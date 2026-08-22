ef8496d2c181514f9c568bb3033a23619a594870
Traveller-5040 <gabrielcruztorreblanca@gmail.com>
2026-08-22 00:01:01 -0500

    Add Act5_b.cpp to print array elements

---
+++ lab1/actividades/Act5_b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5}; // arreglo de enteros, tamano fijo tambien
+  // recorremos el arreglo con un for normal
+  for (int i = 0; i < 5; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
