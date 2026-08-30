cad498d6040f3bc38698910b1ebf16a76cc1a92d
Traveller-5040 <gabrielcruztorreblanca@gmail.com>
2026-08-29 19:01:16 -0500

    Create Categoria.java

---
+++ lab2/SistemaDeMatricula/gestion/Categoria.java
@@ -0,0 +1,11 @@
+package gestion;
+
+public class Categoria {
+    private final String nombre;
+
+    public Categoria(String n) {
+        this.nombre = n;
+    }
+
+    public String getNombre() { return this.nombre; }
+}
